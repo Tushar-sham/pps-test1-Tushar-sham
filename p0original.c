a925768f3af361ca606756b2975df670631644c6
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2021-11-26 09:01:22 +0000

    scanf

--- p0original.c
+++ p0original.c
@@ -1,1 +1,12 @@
- 
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("enter the sum of two numbers\n");
+  scanf("%d %d",& a,&b);
+  sum = a+b;
+  printf("%d+%d=%d\n",a,b,sum);
+ return(0);
+}
+
+
