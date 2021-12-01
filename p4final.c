298d024b139c9a312ff4561293b3072c93f151c4
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2021-12-01 15:13:20 +0000

    void output(int large) printf("the largest element is: %d\n" , large); int calculate

---
+++ p4final.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int input(){
+  int m;
+  printf("enter a number:");
+  scanf("%d" , &m);
+  return m;
+}
+
+int calculate (int m,int n,int p){if (m>n){
+  if(m>p){
+    return m;
+  }else{
+    return p;
+  }
+}else{
+  if(n>p){
+    return n;
+  }else{
+    return p;
+  }
+}
+}
+
+void output(int large){
+  printf("the largest element is: %d\n", large);
+}
+int main(){
+  int m,n,p,large;
+  m = input();
+  n = input();
+  p = input();
+  large = calculate(m,n,p);
+  output(large);
+  return 0;
+}
