84909cc8a0547694ccb9a47d148005136bb266ab
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2021-12-07 15:19:02 +0000

    int calc(int n) for(int i=1; i<=n; i++) scaf("%d", &a);

--- p2final.c
+++ p2final.c
@@ -1,28 +1,28 @@
-# include<stdio.h>
-
+#include <stdio.h>
 int input()
 {
   int a;
-  printf("enter two numbers\n");
+  printf("enter a number\n");
   scanf("%d",&a);
   return a;
 }
 int add(int m, int n)
 {
   int sum;
-  sum=m+n;
+  sum  = m+n;
   return sum;
 }
 int output(int a,int b,int g)
 {
-printf("sum of the two numbers %d and %d is = %d\n",a,b,g);
-return 0;
+  printf("sum of two numbers %d and %d is = %d\n",a,b,g);
+  return 0;
 }
 int main()
 {
   int h,n1,n2;
-  n1=input();
-  n2=input();
-  h=add(n1,n2);
-  output(n1, n2, h);
-}
+   n1 = input();
+   n2 = input();
+   h = add(n1,n2);
+   output (n1 ,n2 ,h);
+   }
+   
