3002b39aaadff0e6f45dc61bd03814be0eea56b9
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2021-12-08 15:12:31 +0000

    int sum = sum 'n'  arrays(n, a);

---
+++ p7original.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int input_size_array(){
+  int n;
+  printf("enter the number of input: ");
+  scanf("%d" , &n);
+  return n;
+}
+
+void input_array(int n, int a[n]){
+  for(int i=0; i<n; i++){
+    printf("enter a number: ");
+    scanf("%d", &a[i]);
+  }
+}
+
+int sum_n_arrays(int n, int a[n]){
+  int sum=0;
+  for(int i=0; i<n; i++){
+    sum += a[i];
+  }
+  return sum;
+}
+
+void output(int n, int a[n], int sum){
+  for(int i=0; i<n-1; i++){
+    printf("%d+" , a[i]);
+  }
+printf("%d is %d\n" , a[n-1], sum);
+}
+int main()
+{
+  int n =input_size_array();
+  int a[n];
+  input_array(n, a);
+  int sum = sum 'n' arrays(n, a);
+  output (n, a, sum);
+  return 0;
+}  
