1417c492265ea5ffc5d2f1ceb3c1929fc9dcca03
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2021-12-20 15:03:25 +0000

    nothing

--- p9original.c
+++ p9original.c
@@ -1,1 +1,21 @@
- 
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char str1[20];
+  char str2[20];
+  int value;
+  printf("enter the first string : ");
+  scanf("%s",str1);
+  printf("enter the second string : ");
+  scanf("%s",str2);
+
+  value=strcmp(str1,str2);
+  if(value==0)
+  printf("string are same");
+  else
+  printf("string are not same");
+  return 0;
+}
+
