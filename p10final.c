7bea664f4d41fcd5a90ee673e4a5bbb4fd099d0d
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2022-01-03 06:32:52 +0000

    int main()

--- p10final.c
+++ p10final.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+struct complex 
+{
+  int real, img;
+};
+int main()
+  {
+    int num= 1,count;
+    float sum=0.0, fact;
+    while (num<= 7)
+    {
+      fact=1;
+      for(count=1; count<= num; count++)
+      {
+        fact = fact* count;
+      }
+  sum =sum+(num/fact);
+  num++;
+    }
+    printf("sum of series is %f\n",sum);
+    return 0;
+  }
--- p10original.c
+++ p10original.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
-
-struct complexnumber 
+#include<stdio.h>
+struct complex 
 {
   int real, img;
 };
-struct complex number 
-complex, add(complex a, complex b)
-{
-  struct complexnum c;
-
-  c.real = a.real + b.real;
-  c.img = a.img + b.img;
-  return c;
-}
 int main()
-{
-  struct complexnumber a = {1, 2};
-  struct complexnumber b = {4, 5};
-  struct complexnumber c = sumcomplex(a,b);
-  printf("complex number 1: i%d\n",a.real, a.img);
-  printf("complex number 2: %d + i%d\n",b.real, b.img);
-  printf("sum of the complex numbers: %d + i%d\n",c.real,c.img);
-  return 0;
-}
-
+  {
+    int num= 1,count;
+    float sum=0.0, fact;
+    while (num<= 7)
+    {
+      fact=1;
+      for(count=1; count<= num; count++)
+      {
+        fact = fact* count;
+      }
+  sum =sum+(num/fact);
+  num++;
+    }
+    printf("sum of series is %f\n",sum);
+    return 0;
+  }
